3b43519059fb41f9a323125ac5e308b8c58a6bae
Matthieu Viry <matthieu.viry@univ-grenoble-alpes.fr>
2022-09-17 10:59:27 +0200

    Update example to use latest contour-rs version + colorbrewer rust library

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,16 +1,37 @@
 extern crate wasm_bindgen;
 extern crate geojson;
 extern crate contour;
+extern crate colorbrewer;
 use wasm_bindgen::prelude::*;
-use geojson::{FeatureCollection, GeoJson};
+use geojson::{FeatureCollection, GeoJson, JsonValue};
+use colorbrewer::{Palette, get_color_ramp};
 use contour::ContourBuilder;
+
+mod utils {
+    pub fn set_panic_hook() {
+        #[cfg(feature = "console_error_panic_hook")]
+        console_error_panic_hook::set_once();
+    }
+}
 
 #[wasm_bindgen]
 pub fn make_contours(values: &[f64], thresholds: &[f64], x: u32, y: u32) -> String {
+    utils::set_panic_hook();
+    let n = thresholds.len();
+    // Get a Vec of colors from colorbrewer PuOr Palette
+    let colors = get_color_ramp(Palette::PiYG, n as u32).unwrap();
+    // Build the contours
     let c = ContourBuilder::new(x, y, true);
+    let mut features = c.contours(values, thresholds).unwrap();
+    // Iterate over features to add color property
+    for (i, f) in features.iter_mut().enumerate() {
+        let color: JsonValue = colors[i].to_string().into();
+        f.properties.as_mut().unwrap().insert("color".to_string(), color);
+    }
+    // Return result serialised as GeoJSON
     GeoJson::from(FeatureCollection {
         bbox: None,
-        features: c.contours(values, thresholds),
+        features: features,
         foreign_members: None,
     }).to_string()
 }
--- js/index.js
+++ js/index.js
@@ -1,42 +1,52 @@
-import module from '../crate/Cargo.toml'
+import wasmModule from '../crate/Cargo.toml'
 import * as d3 from 'd3';
 
-const value = (x, y) => Math.sin(x + y) * Math.sin(x - y);
-
-const canvas = d3.select(document.body)
-.append('canvas')
-.attr('width', 600)
-.attr('height', 600)
-.node();
+const canvas = d3.select('.canvas-container')
+  .append('canvas')
+  .attr('width', 600)
+  .attr('height', 500)
+  .node();
 const context = canvas.getContext("2d");
-const color = d3.scaleSequential(d3.interpolateYlGnBu).domain([-1, 1]);
 const path = d3.geoPath(null, context);
-const thresholds = d3.range(-1.2, 1, 0.2);
 
-const n = 120;
-const m = 120;
+const n = 140;
+const m = 140;
 const _values = new Float64Array((n + 2) * (m + 2));
 
+const value = (x, y) => Math.sin(x + y) * Math.sin(x - y);
 for (var j = -0.5, k = 0; j < m + 1; ++j) {
-for (var i = -0.5; i < n + 1; ++i, ++k) {
-_values[k] = value(i / n * 19.2 - 9.6, 5 - j / m * 10);
+  for (var i = -0.5; i < n + 1; ++i, ++k) {
+    _values[k] = value(i / n * 19.2 - 9.6, 5 - j / m * 15);
+  }
 }
-}
+const thresholds = d3.range(-1.2, 1.2, 0.22);
+const _dv = (t) => (t % 1200) / 1200 * 0.22
 
-context.scale(canvas.width / (n + 1), canvas.width / (n + 1));
+// const value = (x, y) => 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y;
+// for (var j = 0; j < m + 2; ++j) {
+//   for (var i = 0; i < n + 2; ++i) {
+//     _values[j * (n + 2) + i] = value(i / 7 - 10.5, m / 7 - j / 7 - 10.5);
+//   }
+// }
+// const thresholds = d3.range(-1, 10, 1);
+// const _dv = (t) => (t % 1000) / 1000 * 2;
+
+
+context.scale(canvas.width / (n + 2), canvas.width / (n + 2));
 context.translate(-0.5, -0.5);
 
 d3.timer((t) => {
-const dv = (t % 1000) / 1000 * 0.2;
-const n_thresholds = new Float64Array(thresholds.map(v => v + dv));
-const result = JSON.parse(module.make_contours(_values, n_thresholds, n + 2, m + 2));
-result.features.forEach(fill);
+  const dv = _dv(t);
+  const n_thresholds = new Float64Array(thresholds.map(v => v + dv));
+  console.log(n_thresholds);
+  const result = JSON.parse(wasmModule.make_contours(_values, n_thresholds, n + 2, m + 2));
+  result.features.forEach(fill);
 });
 
 function fill(ft) {
-context.beginPath();
-path(ft.geometry);
-context.fillStyle = color(ft.properties.value);
-context.fill();
+  context.beginPath();
+  path(ft.geometry);
+  context.fillStyle = ft.properties.color
+  context.fill();
 }
 
