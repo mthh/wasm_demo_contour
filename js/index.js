fd79d4a4220ae8c10917fc97753ca1c77d5401ad
Matthieu Viry <matthieu.viry@univ-grenoble-alpes.fr>
2022-10-01 10:41:57 +0200

    Remove some js dependencies and update metadata

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 import wasmModule from '../crate/Cargo.toml'
-import * as d3 from 'd3';
+import { range as d3range } from 'd3-array';
+import { geoPath as d3geoPath } from 'd3-geo';
+import { timer as d3timer } from 'd3-timer';
 
-const canvas = d3.select('.canvas-container')
-  .append('canvas')
-  .attr('width', 600)
-  .attr('height', 500)
-  .node();
+const container = document.querySelector('.canvas-container');
+const canvas = document.createElement('canvas');
+canvas.setAttribute('width', 600);
+canvas.setAttribute('height', 500);
+container.appendChild(canvas);
+
 const context = canvas.getContext("2d");
-const path = d3.geoPath(null, context);
+const path = d3geoPath(null, context);
 
 const n = 140;
 const m = 140;
@@ -19,7 +22,7 @@ for (var j = -0.5, k = 0; j < m + 1; ++j) {
     _values[k] = value(i / n * 19.2 - 9.6, 5 - j / m * 15);
   }
 }
-const thresholds = d3.range(-1.2, 1.2, 0.22);
+const thresholds = d3range(-1.2, 1.2, 0.22);
 const _dv = (t) => (t % 1200) / 1200 * 0.22
 
 // const value = (x, y) => 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y;
@@ -28,14 +31,14 @@ const _dv = (t) => (t % 1200) / 1200 * 0.22
 //     _values[j * (n + 2) + i] = value(i / 7 - 10.5, m / 7 - j / 7 - 10.5);
 //   }
 // }
-// const thresholds = d3.range(-1, 10, 1);
+// const thresholds = d3range(-1, 10, 1);
 // const _dv = (t) => (t % 1000) / 1000 * 2;
 
 
 context.scale(canvas.width / (n + 2), canvas.width / (n + 2));
 context.translate(-0.5, -0.5);
 
-d3.timer((t) => {
+d3timer((t) => {
   const dv = _dv(t);
   const n_thresholds = new Float64Array(thresholds.map(v => v + dv));
   console.log(n_thresholds);
